49cefbbea8326c853ef674ef97a88b6e4d84ab90
Chris <Cballenger3@gatech.edu>
2023-03-10 02:35:58 -0500

    Map shows on home screen.

---
+++ components/PurpleButton.tsx
@@ -0,0 +1,38 @@
+import { StyleSheet, View, Pressable, Text } from 'react-native';
+
+export default function Button({ label, onPress }) {
+  return (
+    <View style={styles.buttonContainer}>
+      <Pressable style={styles.button} onPress={onPress}>
+        <Text style={styles.buttonLabel}>{label}</Text>
+      </Pressable>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  buttonContainer: {
+    width: 320,
+    height: 68,
+    marginHorizontal: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 3,
+  },
+  button: {
+    borderRadius: 50,
+    width: '100%',
+    height: '100%',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    backgroundColor: 'purple'
+  },
+  buttonIcon: {
+    paddingRight: 8,
+  },
+  buttonLabel: {
+    color: '#fff',
+    fontSize: 16,
+  },
+});
--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
 import { Input, NativeBaseProvider, Button, Icon, Box, Image, AspectRatio } from 'native-base';
 import { FontAwesome5 } from '@expo/vector-icons';
 import { useNavigation } from '@react-navigation/native';
@@ -8,15 +8,74 @@ import { alignContent, flex, flexDirection, width } from 'styled-system';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RootStackParamList } from '../App';
 
+import { WebView } from 'react-native-webview';
+
+import PurpleButton from '../components/PurpleButton'
+
 type loginScreenProp = StackNavigationProp<RootStackParamList, 'Login'>;
+
+const position = [51.505, -0.09];
 
 function Home() {
     const navigation = useNavigation<loginScreenProp>();
+
+    const markers = [
+        { lat: 51.505, lng: -0.09, name: 'Marker 1' },
+        { lat: 51.51, lng: -0.1, name: 'Marker 2' },
+        { lat: 51.495, lng: -0.1, name: 'Marker 3' },
+    ];
+
+    const mapHtml = `
+    <html>
+          <head>
+            <link rel="stylesheet" href="https://unpkg.com/leaflet@1.7.1/dist/leaflet.css" />
+            <script src="https://unpkg.com/leaflet@1.7.1/dist/leaflet.js"></script>
+          </head>
+          <body>
+            <div id="map" style="height: 100%;" style="scale: 2"></div>
+            <script>
+              var map = L.map('map').setView([33.75, -84.39], 13);
+              L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                attribution: '&copy; <a href="https://www.o penstreetmap.org/">OpenStreetMap</a> contributors',
+                maxZoom: 19,
+              }).addTo(map);
+              
+            ${markers.map((marker) => `
+                        L.marker([${marker.lat}, ${marker.lng}])
+                          .bindPopup('${marker.name}')
+                          .addTo(map);
+                      `).join('')}
+
+            </script>
+          </body>
+        </html>
+      `;
+
+      console.log(mapHtml);
+
     return (
+        // <View style={styles.container}>
+        //     <LeafletView
+        //         style={{ flex: 1 }}
+        //         initialRegion={{
+        //             latitude: 37.78825,
+        //             longitude: -122.4324,
+        //             latitudeDelta: 0.0922,
+        //             longitudeDelta: 0.0421
+        //         }}
+        //     />
+        //  </View>
+
         <View style={styles.container}>
-            <View style={styles.Middle}>
-                <Text style={styles.HomeText}>Home Screen</Text>
-            </View>
+
+              <WebView
+                style={styles.mapView}
+                source={{ html: mapHtml }}
+              />
+              <TextInput style={styles.inputContainer} placeholder="Find your friends" />
+              <View style={styles.button}>
+                <PurpleButton label="Share my location" />
+              </View>
         </View>
     )
 
@@ -45,5 +104,29 @@ const styles = StyleSheet.create({
     Middle: {
         alignItems: 'center',
         justifyContent: 'center',
+    },
+    mapView: {
+      transform: [{scale: 2}]
+    },
+    inputContainer: {
+        position: 'absolute',
+        top: 50,
+        left: 10,
+        right: 10,
+        width: 365,
+        height: 50,
+        borderWidth: 1,
+        backgroundColor: 'white',
+        borderRadius: 50,
+        padding: 5,
+        paddingLeft: 15
+    },
+    button: {
+      position: 'absolute',
+      bottom: 100,
+      left: 10,
+      right: 10,
+      width: 365,
+      height: 50
     }
 });
